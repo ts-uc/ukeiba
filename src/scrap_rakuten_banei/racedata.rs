5a94736cecfb9e13dd4a13f5246e26dcec52421e
Nono Yuu <s0@ts-uc.org>
2022-10-18 22:39:21 +0900

    racedata.rs の各関数をトレイト化

--- src/scrap_rakuten_banei/racedata.rs
+++ src/scrap_rakuten_banei/racedata.rs
@@ -20,39 +20,69 @@ pub struct RaceDataBanei {
 }
 
 pub trait RaceDataBaneiTrait {
+    fn scrap_str(&self, selectors: &str) -> String;
+    fn scrap_weather(&self) -> String;
+    fn scrap_track_condition(&self) -> String;
+    fn scrap_post_time(&self) -> String;
+    fn scrap_name(&self) -> String;
+    fn scrap_class(&self) -> String;
+    fn horse_condition(&self) -> String;
     fn scrap_race_data(&self, race: Race) -> RaceDataBanei;
 }
 
 impl RaceDataBaneiTrait for Html {
-    fn scrap_race_data(&self, race: Race)-> RaceDataBanei {
-        let track_main_state = scrap_str(".raceNote > h2:nth-child(3)", &self);
+    fn scrap_str(&self, selectors: &str) -> String {
+        let weatcher_selector = Selector::parse(selectors).unwrap();
+        self.select(&weatcher_selector)
+            .next()
+            .unwrap()
+            .text()
+            .collect::<Vec<_>>()[0]
+            .nfkc()
+            .collect::<String>()
+    }
 
+    fn scrap_weather(&self) -> String {
+        self.scrap_str(
+            "ul.trackState:nth-child(2) > li:nth-child(2) > dl:nth-child(1) > dd:nth-child(2)",
+        )
+    }
+
+    fn scrap_track_condition(&self) -> String {
+        self.scrap_str(
+            "ul.trackState:nth-child(2) > li:nth-child(2) > dl:nth-child(1) > dd:nth-child(4)",
+        )
+    }
+
+    fn scrap_post_time(&self) -> String {
+        self.scrap_str(
+            "ul.trackState:nth-child(2) > li:nth-child(2) > dl:nth-child(1) > dd:nth-child(6)",
+        )
+    }
+
+    fn scrap_name(&self) -> String {
+        self.scrap_str(".raceNote > h2:nth-child(3)")
+    }
+
+    fn scrap_class(&self) -> String {
+        self.scrap_name()
+    }
+    
+    fn horse_condition(&self) -> String {
+        self.scrap_str(".horseCondition > li:nth-child(1)")
+    }
+
+    fn scrap_race_data(&self, race: Race) -> RaceDataBanei {
         RaceDataBanei {
             date: race.date,
             racecourse: race.racecourse,
             num: race.num,
-            weather: scrap_str(
-                "ul.trackState:nth-child(2) > li:nth-child(2) > dl:nth-child(1) > dd:nth-child(2)",
-                &self,
-            ),
-            track_condition: scrap_str(
-                "ul.trackState:nth-child(2) > li:nth-child(2) > dl:nth-child(1) > dd:nth-child(4)",
-                &self,
-            ),
-            post_time: scrap_str(
-                "ul.trackState:nth-child(2) > li:nth-child(2) > dl:nth-child(1) > dd:nth-child(6)",
-                &self,
-            ),
-            name: track_main_state.clone(),
-            class: track_main_state.clone(),
-            horse_condition: scrap_str(".horseCondition > li:nth-child(1)", &self),
+            weather: self.scrap_weather(),
+            track_condition: self.scrap_track_condition(),
+            post_time: self.scrap_post_time(),
+            name: self.scrap_name(),
+            class: self.scrap_class(),
+            horse_condition: self.horse_condition(),
         }
     }
-}
-
-pub fn scrap_str(selectors: &str, doc: &Html) -> String {
-    let weatcher_selector = Selector::parse(selectors).unwrap();
-    let hoge = doc.select(&weatcher_selector).next().unwrap();
-    let r = hoge.text().collect::<Vec<_>>()[0];
-    r.nfkc().collect::<String>()
 }
