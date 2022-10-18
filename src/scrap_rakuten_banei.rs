a1144046d3f6bd01d469b2d81e312a739cd31849
Nono Yuu <s0@ts-uc.org>
2022-10-18 21:02:08 +0900

    scrap_rakuten_banei の関数を整理

--- src/scrap_rakuten_banei.rs
+++ src/scrap_rakuten_banei.rs
@@ -5,8 +5,13 @@ extern crate unicode_normalization;
 use super::Race;
 use super::Racecourse;
 
+enum PageType {
+    Odds,
+    Results,
+}
+
 impl Race {
-    fn race_id(&self) -> String {
+    fn get_race_id(&self) -> String {
         format!(
             "{}{:02}000000{:02}",
             self.date.format("%Y%m%d"),
@@ -16,36 +21,32 @@ impl Race {
             self.num
         )
     }
-    fn url_result(&self) -> String {
-        format!(
-            "https://keiba.rakuten.co.jp/race_performance/list/RACEID/{}",
-            self.race_id()
-        )
+
+    fn get_url(&self, page_type: &PageType) -> String {
+        match page_type {
+            PageType::Odds => format!(
+                "https://keiba.rakuten.co.jp/odds/tanfuku/RACEID/{}",
+                self.get_race_id()
+            ),
+            PageType::Results => format!(
+                "https://keiba.rakuten.co.jp/race_performance/list/RACEID/{}",
+                self.get_race_id()
+            ),
+        }
     }
-    fn url_odds(&self) -> String {
-        format!(
-            "https://keiba.rakuten.co.jp/odds/tanfuku/RACEID/{}",
-            self.race_id()
-        )
-    }
-    fn fetch_result(&self) -> String {
-        eprintln!("Fetching {:?}...", self.url_result());
-        let res = reqwest::blocking::get(self.url_result()).unwrap();
-        eprintln!("Response: {:?} {}", res.version(), res.status());
-        res.text().unwrap().to_string()
-    }
-    fn fetch_odds(&self) -> String {
-        eprintln!("Fetching {:?}...", self.url_odds());
-        let res = reqwest::blocking::get(self.url_odds()).unwrap();
+
+    fn fetch(&self, page_type: PageType) -> String {
+        eprintln!("Fetching {:?}...", self.get_url(&page_type));
+        let res = reqwest::blocking::get(self.get_url(&page_type)).unwrap();
         eprintln!("Response: {:?} {}", res.version(), res.status());
         res.text().unwrap().to_string()
     }
 }
 
 pub fn scrap(race: Race) -> Result<(), Box<dyn std::error::Error>> {
-    let body: String = race.fetch_result();
+    let body_odds: String = race.fetch(PageType::Odds);
 
-    let racedata = racedata::RaceDataBanei::scrap(&body, race);
+    let racedata = racedata::RaceDataBanei::scrap(&body_odds, race);
     println!("{:?}", racedata);
 
     Ok(())
