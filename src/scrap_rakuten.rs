863a682b4b9930427fca58c5871a5ca7ae7b9109
Nono Yuu <s0@ts-uc.org>
2022-10-22 09:00:39 +0900

    帯広競馬場以外だったときの処理を_に変更

--- src/scrap_rakuten.rs
+++ src/scrap_rakuten.rs
@@ -20,7 +20,7 @@ impl Race {
             self.date.format("%Y%m%d"),
             match self.racecourse {
                 Racecourse::Obihiro => 3,
-                Racecourse::Monbetsu => todo!()
+                _ => todo!()
             },
             self.num
         )
