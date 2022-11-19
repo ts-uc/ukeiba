b523ea224d0cf1bf9e30d3cd16321ee3aaf26c94
Nono Yuu <s0@ts-uc.org>
2022-11-20 00:12:06 +0900

    create.sql を改良

--- db/create.sql
+++ db/create.sql
@@ -1,14 +1,13 @@
-CREATE TABLE race (
+CREATE TABLE races (
     race_id INTEGER PRIMARY KEY,
     race_date TEXT NOT NULL,
-    racecource TEXT NOT NULL,
+    racecourse TEXT NOT NULL,
     race_num INTEGER NOT NULL,
-    posttime TEXT,
+    post_time TEXT,
     change TEXT,
     race_type TEXT,
     race_name TEXT,
-    class TEXT,
-    surface TEXT,
+    surface TEXT,               
     direction TEXT,
     distance INTEGER,
     weather TEXT,
@@ -19,38 +18,89 @@ CREATE TABLE race (
     updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
 );
 
-CREATE TRIGGER trigger_race_updated_at AFTER UPDATE ON race
+CREATE TRIGGER trigger_races_updated_at AFTER UPDATE ON races
 BEGIN
-    UPDATE race SET updated_at = DATETIME('now', 'localtime') WHERE rowid == NEW.rowid;
+    UPDATE races SET updated_at = DATETIME('now', 'localtime') WHERE rowid == NEW.rowid;
 END;
 
-CREATE TABLE result (
+CREATE TABLE race_horses (
     race_horse_id INTEGER PRIMARY KEY,
     race_id INTEGER NOT NULL,
     horse_num INTEGER NOT NULL,
     bracket_num INTEGER,
     arrival INTEGER,
-    horse_name TEXT,
     horse_id INTEGER,
-    horse_affiliation TEXT,
     horse_sex TEXT,
     horse_age INTEGER,
     weight_to_carry INTEGER,
-    jockey TEXT,
     jockey_id INTEGER,
-    trainer TEXT,
     trainer_id INTEGER,
     horse_weight INTEGER,
     horse_weight_delta INTEGER,
-    finish REAL,
+    finish_time REAL,
+    margin_time REAL,
     margin TEXT,
-    three_furlongs REAL,
+    last_3f REAL,
     win_fav INTEGER,
+    win_odds REAL,
+    place_odds_min REAL,
+    place_odds_max REAL,
+    prize INTEGER,
     created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
     updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
 );
 
-CREATE TRIGGER trigger_result_updated_at AFTER UPDATE ON result
+CREATE TRIGGER trigger_race_horses_updated_at AFTER UPDATE ON race_horses
 BEGIN
-    UPDATE result SET updated_at = DATETIME('now', 'localtime') WHERE rowid == NEW.rowid;
+    UPDATE race_horses SET updated_at = DATETIME('now', 'localtime') WHERE rowid == NEW.rowid;
+END;
+
+CREATE TABLE horses (
+    horse_id INTEGER PRIMARY KEY,
+    horse_name TEXT,
+    sire_name TEXT,
+    dam_name TEXT,
+    sires_sire_name TEXT,
+    sires_dam_name TEXT,
+    dams_sire_name TEXT,
+    dams_dam_name TEXT,
+    breeder TEXT,
+    birthplace TEXT,
+    created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+    updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+);
+
+CREATE TRIGGER trigger_horses_updated_at AFTER UPDATE ON horses
+BEGIN
+    UPDATE horses SET updated_at = DATETIME('now', 'localtime') WHERE rowid == NEW.rowid;
+END;
+
+CREATE TABLE jockeys (
+    jockey_id INTEGER PRIMARY KEY,
+    jockey_name TEXT,
+    jockey_sex TEXT,
+    jockey_status TEXT,
+    jockey_affiliation TEXT,
+    created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+    updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+);
+
+CREATE TRIGGER trigger_jockeys_updated_at AFTER UPDATE ON jockeys
+BEGIN
+    UPDATE jockeys SET updated_at = DATETIME('now', 'localtime') WHERE rowid == NEW.rowid;
+END;
+
+CREATE TABLE trainers (
+    trainer_id INTEGER PRIMARY KEY,
+    trainer_name TEXT,
+    trainer_sex TEXT,
+    trainer_status TEXT,
+    trainer_affiliation TEXT,
+    created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+    updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+);
+
+CREATE TRIGGER trigger_trainers_updated_at AFTER UPDATE ON trainers
+BEGIN
+    UPDATE trainers SET updated_at = DATETIME('now', 'localtime') WHERE rowid == NEW.rowid;
 END;
