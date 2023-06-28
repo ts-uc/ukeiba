047fbfa896fd7e1706b0a7f115a936a0efdb9f42
Nono Yuu <s0@ts-uc.org>
2023-06-28 22:48:40 +0900

    DBのカラムの順序を変更

--- ukeiba-test/src/db.rs
+++ ukeiba-test/src/db.rs
@@ -4,56 +4,55 @@ use rusqlite::{params, Connection};
 use serde::{Deserialize, Serialize};
 pub mod writer;
 
-// 以下の構造体に基づいて、rusqliteでcreate tableする関数を作成してください。
-// なお、サロゲートキーは使わず、optionのついていないメンバ変数を主キーにしてください。
-// また、外部キー制約を用いてください。
+// 以下の構造体に基づいて、SQLITE でCREATE TABLE をするSQL文を作成してください。
+// なお、サロゲートキーは使わないでください。
 // CREATE TABLE文は、IF NOT EXISTS を使ったものにしてください。
 // テーブル名、およびカラム名はすべてスネークケースにしてください。
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct Dates {
-    pub date: NaiveDate,
+    pub date: NaiveDate, // 主キー
     pub racecourse: Option<String>,
     pub fiscal_year: Option<i32>,
-    pub kai: Option<String>,
-    pub nichi: Option<String>,
+    pub kai: Option<i32>,
+    pub nichi: Option<i32>,
 }
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct Races {
-    pub date: NaiveDate,
-    pub race_num: i32,
-    pub race_type: Option<String>,
-    pub weather: Option<String>,
-    pub going: Option<f64>,
-    pub horse_count: Option<i32>,
+    pub date: NaiveDate, // 主キー・datesテーブルの外部キー
+    pub race_num: i32,   // 主キー
     pub post_time: Option<NaiveTime>,
     pub post_time_change: Option<bool>,
     pub race_sub_name: Option<String>,
     pub race_name: Option<String>,
+    pub weather: Option<String>,
+    pub going: Option<f64>,
     pub race_weight_type: Option<String>,
+    pub race_type: Option<String>,
+    pub horse_count: Option<i32>,
 }
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct RaceHorses {
-    pub date: NaiveDate,
-    pub race_num: i32,
-    pub horse_num: i32,
+    pub date: NaiveDate, // 主キー・racesテーブルの外部キー
+    pub race_num: i32,   // 主キー・racesテーブルの外部キー
+    pub horse_num: i32,  // 主キー
     pub horse_nar_id: Option<i64>,
     pub bracket_num: Option<i32>,
-    pub win_fav: Option<i32>,
-    pub horse_weight: Option<i32>,
+    pub horse_sex: Option<String>,
     pub jockey_id: Option<i32>,
+    pub weight_mark: Option<String>,
     pub weight_to_carry: Option<i32>,
     pub trainer_id: Option<i32>,
+    pub owner_name: Option<String>,
+    pub horse_weight: Option<i32>,
+    pub change: Option<String>,
+    pub win_fav: Option<i32>,
     pub arrival: Option<i32>,
     pub arrival_info: Option<String>,
     pub finish_time: Option<f64>,
     pub prize: Option<i32>,
-    pub change: Option<String>,
-    pub horse_sex: Option<String>,
-    pub weight_mark: Option<String>,
-    pub owner_name: Option<String>,
     pub win_odds: Option<f64>,
     pub place_odds_min: Option<f64>,
     pub place_odds_max: Option<f64>,
@@ -61,8 +60,8 @@ pub struct RaceHorses {
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct Horses {
-    pub horse_nar_id: Option<i64>,
-    pub horse_bajikyo_id: Option<String>,
+    pub horse_bajikyo_id: Option<String>, // 主キー
+    pub horse_nar_id: Option<i64>,        // ユニークキー
     pub horse_name: Option<String>,
     pub horse_status: Option<String>,
     pub deregistration_date: Option<NaiveDate>,
@@ -78,7 +77,7 @@ pub struct Horses {
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct Jockeys {
-    pub jockey_nar_id: i32,
+    pub jockey_nar_id: i32, // 主キー
     pub name: String,
     pub kana: String,
     pub sex: String,
@@ -90,7 +89,7 @@ pub struct Jockeys {
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct Trainers {
-    pub trainer_nar_id: i32,
+    pub trainer_nar_id: i32, // 主キー
     pub name: String,
     pub kana: String,
     pub sex: String,
@@ -108,73 +107,61 @@ pub fn make_conn() -> Result<Connection> {
 
 pub fn create_table() -> Result<()> {
     let conn = make_conn()?;
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS dates (
-            date DATE NOT NULL PRIMARY KEY,
+    conn.execute_batch(
+        "
+        CREATE TABLE IF NOT EXISTS dates (
+            date TEXT PRIMARY KEY,
             racecourse TEXT,
             fiscal_year INTEGER,
-            kai TEXT,
-            nichi TEXT
-        )",
-        params![],
-    )?;
-
-    // Racesテーブルの作成
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS races (
-            date DATE NOT NULL,
-            race_num INTEGER NOT NULL,
-            race_type TEXT,
-            weather TEXT,
-            going REAL,
-            horse_count INTEGER,
+            kai INTEGER,
+            nichi INTEGER
+        );
+        
+        CREATE TABLE IF NOT EXISTS races (
+            date TEXT,
+            race_num INTEGER,
             post_time TEXT,
             post_time_change INTEGER,
             race_sub_name TEXT,
             race_name TEXT,
+            weather TEXT,
+            going REAL,
             race_weight_type TEXT,
+            race_type TEXT,
+            horse_count INTEGER,
             PRIMARY KEY (date, race_num),
-            FOREIGN KEY (date) REFERENCES dates (date)
-        )",
-        params![],
-    )?;
-
-    // RaceHorsesテーブルの作成
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS race_horses (
-            date DATE NOT NULL,
-            race_num INTEGER NOT NULL,
-            horse_num INTEGER NOT NULL,
+            FOREIGN KEY (date) REFERENCES dates(date)
+        );
+        
+        CREATE TABLE IF NOT EXISTS race_horses (
+            date TEXT,
+            race_num INTEGER,
+            horse_num INTEGER,
             horse_nar_id INTEGER,
             bracket_num INTEGER,
-            win_fav INTEGER,
-            horse_weight INTEGER,
+            horse_sex TEXT,
             jockey_id INTEGER,
+            weight_mark TEXT,
             weight_to_carry INTEGER,
             trainer_id INTEGER,
+            owner_name TEXT,
+            horse_weight INTEGER,
+            change TEXT,
+            win_fav INTEGER,
             arrival INTEGER,
             arrival_info TEXT,
             finish_time REAL,
             prize INTEGER,
-            change TEXT,
-            horse_sex TEXT,
-            weight_mark TEXT,
-            owner_name TEXT,
             win_odds REAL,
             place_odds_min REAL,
             place_odds_max REAL,
             PRIMARY KEY (date, race_num, horse_num),
-            FOREIGN KEY (date, race_num) REFERENCES races (date, race_num)
-        )",
-        params![],
-    )?;
-
-    // Horsesテーブルの作成
-    conn.execute(
-        "
+            FOREIGN KEY (date, race_num) REFERENCES races(date, race_num)
+        );
+        
         CREATE TABLE IF NOT EXISTS horses (
+            horse_bajikyo_id TEXT PRIMARY KEY,
             horse_nar_id INTEGER UNIQUE,
-            horse_bajikyo_id TEXT UNIQUE,
             horse_name TEXT,
             horse_status TEXT,
             deregistration_date TEXT,
@@ -186,40 +173,30 @@ pub fn create_table() -> Result<()> {
             sire_bajikyo_id TEXT,
             dam_bajikyo_id TEXT,
             bms_bajikyo_id TEXT
-        )",
-        [],
-    )?;
-
-    conn.execute(
-        "
+        );
+        
         CREATE TABLE IF NOT EXISTS jockeys (
             jockey_nar_id INTEGER PRIMARY KEY,
             name TEXT,
             kana TEXT,
             sex TEXT,
             status TEXT,
-            birthdate DATE,
-            first_run DATE,
-            first_win DATE
+            birthdate TEXT,
+            first_run TEXT,
+            first_win TEXT
         );
-        ",
-        [],
-    )?;
-
-    conn.execute(
-        "
+        
         CREATE TABLE IF NOT EXISTS trainers (
             trainer_nar_id INTEGER PRIMARY KEY,
             name TEXT,
             kana TEXT,
             sex TEXT,
             status TEXT,
-            birthdate DATE,
-            first_run DATE,
-            first_win DATE
+            birthdate TEXT,
+            first_run TEXT,
+            first_win TEXT
         );
         ",
-        [],
     )?;
 
     Ok(())
