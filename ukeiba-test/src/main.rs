563e27fc27c347ee14ccc1ea6d6dacd87924da85
Nono Yuu <s0@ts-uc.org>
2023-07-15 10:02:35 +0900

    コマンドラインオプションを追加

--- ukeiba-test/src/main.rs
+++ ukeiba-test/src/main.rs
@@ -3,18 +3,43 @@ use db::{create_table, update_race_align, vacuum_database};
 pub mod common;
 pub mod db;
 pub mod get;
+use clap::{Parser, Subcommand};
 pub mod scrap;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    mode: Mode,
+}
+
+#[derive(Subcommand, Debug)]
+enum Mode {
+    Init,
+    Tmp,
+}
+
 fn main() {
+    let args = Args::parse();
+
     create_table().unwrap();
-    scrap::scrap_trainers::scrap();
-    scrap::scrap_jockeys::scrap();
-    scrap::scrap_horse_profile::scrap();
-    scrap::scrap_bajikyo_profile::scrap();
-    scrap::scrap_bajikyo_pedigree::scrap();
-    scrap::scrap_horse_history::scrap();
-    scrap::scrap_horse_table::scrap();
-    scrap::scrap_rakuten_racelist::scrap();
+    match args.mode {
+        Mode::Init => {
+            scrap::scrap_trainers::scrap();
+            scrap::scrap_jockeys::scrap();
+            scrap::scrap_horse_profile::scrap();
+            scrap::scrap_bajikyo_profile::scrap();
+            scrap::scrap_bajikyo_pedigree::scrap();
+            scrap::scrap_horse_history::scrap();
+            scrap::scrap_horse_table::scrap();
+            scrap::scrap_rakuten_racelist::scrap();
+        }
+        Mode::Tmp => {
+            scrap::scrap_horse_history::scrap();
+            scrap::scrap_horse_table::scrap();
+            scrap::scrap_rakuten_racelist::scrap();
+        }
+    }
     update_race_align().unwrap();
     vacuum_database().unwrap();
 }
